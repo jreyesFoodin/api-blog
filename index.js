bd7ccfa21798087236ed3b3011930bedfc116d24
jreyesFoodin <juan.reyes@foodin.mx>
2024-01-13 15:36:37 -0500

    [add] se agrego servicio de get y buscar por id

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 
 const server = require('./src/server')
-console.log(server)
 const port = process.env.PORT || 4088
 server.listen(port, function () {
   console.log(`Server run: http://localhost:${port}`)
--- src/useCases/Blog/index.js
+++ src/useCases/Blog/index.js
@@ -1,18 +1,39 @@
-// const knex = require('../../db/knex')
+const knex = require('../../db/knex')
 
 module.exports = {
   async listBlog () {
     try {
-
+      const response = await knex.select([
+        'idBlog',
+        'title',
+        'author',
+        'publicationDate',
+        'content',
+        'activo'
+      ])
+        .from('blog')
+        .where('activo', 1)
+      return response
     } catch (error) {
-
+      throw new Error(error)
     }
   },
-  async listBlogById () {
+  async listBlogById (id) {
     try {
-
+      const response = await knex.first([
+        'idBlog',
+        'title',
+        'author',
+        'publicationDate',
+        'content',
+        'activo'
+      ])
+        .from('blog')
+        .where('activo', 1)
+        .andWhere('idBlog', id)
+      return response
     } catch (error) {
-
+      throw new Error(error)
     }
   }
 }
